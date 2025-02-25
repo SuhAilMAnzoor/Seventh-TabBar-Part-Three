e33b1ac906a2d6647675779e86b11a34663e20b7
SuhAilMAnzoor <Sohailmanzoorsario@gmail.com>
2025-02-25 23:00:44 +0500

    Seventh Tabbar Part Three, In this project we will create TabBars, this is Part of Seventh-Stack-and-Code-Structures-Part-One Repo

---
+++ android/app/src/main/kotlin/com/example/seventh_tabbar_part_three/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.seventh_tabbar_part_three
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:seventh_tabbar_part_three/screens/Tabbar1/tabbar_one.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+
+  home: const TabbarOne()
+);
+  }
+}
