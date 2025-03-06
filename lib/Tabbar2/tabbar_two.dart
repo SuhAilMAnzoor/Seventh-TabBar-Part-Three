86f3399ead9a93c5e5365e568ecb7c9dde6b490b
SuhAilMAnzoor <Sohailmanzoorsario@gmail.com>
2025-03-06 19:35:20 +0500

    Tabbar two Screen is Completely Okay, and Some Changes are done to Make UI More Beautiful Of Tabbars

--- lib/Tabbar2/tabbar_two.dart
+++ lib/Tabbar2/tabbar_two.dart
@@ -12,33 +12,42 @@ class _TabbarTwoState extends State<TabbarTwo> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, child: Scaffold(
-      appBar: AppBar(
+      length: 3,
+      child: Scaffold(
+       appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0,
+        elevation: 5,
         title: const Text("WhatsApp",
         style: TextStyle(color: Colors.black),),
         bottom: TabBar(
           indicator: BoxDecoration(
-              color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(50)
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(40),
+              boxShadow:[
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                blurRadius: 5, 
+                spreadRadius: 1 // This is Rounded Spread Radius Border Of Selected Tab
+                ),
+               ],
               ),
-              ),
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.black,
+              labelColor: Colors.white, // Selected Indicator Color Shows White
+              unselectedLabelColor: Colors.green,
+              indicatorSize: TabBarIndicatorSize.tab, // Selected Indicator Size 
           tabs: const [
           Tab(
               text: "Chat"),
-          Tab(text: 'Status',),
+          Tab(text: 'Status',),  //These are 3 Tabs those will show at the bottom of Appbar
           Tab(text: "Calls",),
           ]
           ),
       ),
-      body: const TabBarView(children: [
-        Center(child: Text("Chat Screen", style: textStyle)),
+      body: const TabBarView(
+      children: [
+        Center(child: Text("Chat Screen", style: textStyle)), // testStyle Varibale is store above 
         Center(child: Text("Status", style: textStyle,)),
         Center(child: Text("Calls", style: textStyle,)),
-      ]),
+      ]), 
     )
     );
   }
